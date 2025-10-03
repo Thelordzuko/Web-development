1664a5e5596aef83c5c94969fafde5ebe7df3547
Oluwaseyi Alebiosu <alebiosuseyi20@gmail.com>
2025-10-03 14:54:48 +0100

    Completed

---
+++ JavaScript/JavaScript7/Task2/script.js
@@ -0,0 +1,23 @@
+
+class Student{
+    constructor(name, age, grades = []){
+        this.name = name;
+        this.age = age;
+        this.grades = grades;
+    }
+    calculateAverage(){
+        let total = this.grades.reduce((total,  grade) => 
+            total + grade, 0);
+        return total / this.grades.length;
+    }
+
+    getDetails(){
+        let average = this.calculateAverage();
+        let passStatus = average > 50? "Passed": "Failed";
+        return `Student name: ${this.name}. Age: ${this.age}. Average: ${average}. Status: ${passStatus}`
+    }
+}
+
+let student1 = new Student("Seyi", 23, [79, 80])
+
+console.log(student1.getDetails());
---
+++ JavaScript/JavaScript7/Task3/script.js
@@ -0,0 +1,30 @@
+
+class Vehicle{
+    constructor(brand, model, year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+    getInfo(){
+        return `This car is a ${this.brand}. It's model is ${this.model} and it was made in ${this.year}`
+    }
+    isOld(){
+        let check = (2025 - this.year) > 10;
+        return check;
+    }
+}
+
+class Car extends Vehicle{
+    constructor(brand, model, year, fuelType){
+        super(brand, model, year);
+        this.fuelType = fuelType;
+    }
+
+    getInfo(){
+         return `This car is a ${this.brand}. It's model is ${this.model} and it was made in ${this.year} and has a fuel type of ${this.fuelType}`;
+    }
+}
+
+let car1 = new Car("Benz", "Jeep", 2018, "gasoline");
+console.log(car1.getInfo());
+console.log(car1.isOld());
